d02e0660940a927d5d0029edd694ec12fc0804b8
limakanorw <limakanorw@gmail.com>
2025-05-15 09:52:27 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,13 @@ export default function Home() {
   return (
     <div style={{ textAlign: "center", padding: "2rem" }}>
       <h1>🎬 Spersonalizowane Filmiki</h1>
-      <p>Witaj! To testowa wersja strony do zamawiania filmików od "celebrytów".</p>
-      <p>W przyszłości tutaj pojawi się formularz, lista twórców i personalizacja nagrań.</p>
+      <p>Witaj Anno! 🎉</p>
+      <img 
+        src="/celebryta.jpg" 
+        alt="celebryta" 
+        style={{ width: "200px", marginTop: "1rem" }} 
+      />
+      <p>Życzymy Ci wszystkiego najlepszego z okazji urodzin!</p>
     </div>
   );
 }
